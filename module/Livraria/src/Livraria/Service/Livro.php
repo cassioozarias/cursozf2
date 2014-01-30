ac1d4f437e31a30d4bed800cb2bd1ca23c160aa4
cassio <cassio.ozarias@gmail.com>
2014-01-30 08:51:24 -0400

    [ADJUST] ccdcdcd
    Signed-off-by: cassio <cassio.ozarias@gmail.com>

---
+++ module/Livraria/src/Livraria/Service/Livro.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Livraria\Service;
+
+use Doctrine\ORM\EntityManager; 
+use Livraria\Entity\Configurator;
+
+class Livro extends AbstractService {
+   
+    public function __construct(EntityManager $em) {
+        parent::__construct($em);
+        $this->entity = "Livraria\Entity\Livro";
+    }
+    public function insert(array $data) {
+        $entity = new $this->entity($data);
+        
+        $categoria = $this->em->getReference("Livraria\Entity\Categoria" , $data['categoria_id']);       
+        $entity->setCategoria($categoria);
+        
+        $this->em->persist($entity);
+        $this->em->flush();
+        
+        return $entity;
+    }
+    
+    public function update(array $data) {
+      $entity = $this->em->getReference($this->entity, $data['id']);
+      $entity = configurator::configure($entity,$data);
+      
+       $categoria = $this->em->getReference("Livraria\Entity\Categoria" , $data['categoria_id']);       
+       $entity->setCategoria($categoria);
+       
+       $this->em->persist($entity);
+        $this->em->flush();
+        
+        return $entity;
+  }
+}
